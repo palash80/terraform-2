22c648c09c1dc84d43bfff27e51e0c9b244d1fc1
palash80 <153359214+palash80@users.noreply.github.com>
2024-02-17 13:44:02 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region  = "${var.region}"
-  shared_credentials_file = "/home/ubuntu/.aws/credentials"
+  shared_credentials_files = "/home/ubuntu/.aws/credentials"
   profile = "default"
 }
