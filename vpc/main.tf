df154f17ed0ed8fa70e907d3ca6ff22486a375a6
palash80 <153359214+palash80@users.noreply.github.com>
2024-02-25 19:10:15 +0530

    Update main.tf

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,8 +1,10 @@
-
 resource "aws_vpc" "batch24" {
-  cidr_block=var.cidr
-    tags = {
+  cidr_block            = var.cidr
+  enable_dns_support    = true
+  enable_dns_hostnames  = true
+
+  tags = {
     Name = var.name
   }
-  
 }
+
