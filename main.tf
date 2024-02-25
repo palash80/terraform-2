eb38f80d0b0e5946a2d8520af31e0133d4899e6a
palash80 <153359214+palash80@users.noreply.github.com>
2024-02-25 22:57:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -71,6 +71,7 @@ resource "tls_private_key" "rsa" {
 
 resource "local_file" "tf-key" {
   content  = tls_private_key.rsa.private_key_pem
+  file_permission = "0400"
   filename = "./palash414.pem"  # Provide the desired path and file name with .pem extension
 }
 
